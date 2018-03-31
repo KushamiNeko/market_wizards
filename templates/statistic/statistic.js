9baafc9987844f6c0ab54c5b459b677c8a1dfa1d
Kushami Neko <missing10630@gmail.com>
2018-04-01 01:53:21 +0900

    reorganize action view and statistic view

---
+++ templates/statistic/statistic.js
@@ -0,0 +1,53 @@
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$("#input-start-date").focus();
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$("#button-back").click(function() {
+  window.location = "/action";
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+$("#button-query").click(function() {
+
+  $(".invalid-feedback").hide();
+
+  if (isProcessing()) {
+    return;
+  }
+
+  inProcess("#button-query");
+
+  if ($("#input-start-date").val() === "" && $("#input-end-date").val() === "") {
+
+    window.location = "/statistic";
+
+  } else {
+
+    if ($("#input-start-date").val().match(/^\d{8}$/) === null) {
+      $("#validate-start-date").show();
+      $("#input-start-date").focus();
+
+      outProcess("#button-query");
+      return;
+    }
+
+    if ($("#input-end-date").val().match(/^\d{8}$/) === null) {
+      $("#validate-end-date").show();
+      $("#input-end-date").focus();
+
+      outProcess("#button-query");
+      return;
+    }
+
+    window.location = "/statistic?start=" + $("#input-start-date").val() + "&end=" + $("#input-end-date").val();
+
+  }
+
+  outProcess("#button-query");
+
+});
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
