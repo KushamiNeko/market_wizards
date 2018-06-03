e42f52866f323d3178c6c333681583fbad8e201d
Kushami Neko <missing10630@gmail.com>
2018-06-03 22:36:46 +0900

    fix marketsmith quarterly results regex

--- src/marketsmith/parser.go
+++ src/marketsmith/parser.go
@@ -23,7 +23,7 @@ const (
 
 	regexFloatShare = `\s*<div\s*class="companyInfoCenter"\s*[^>]+>\s*<div\s*class="companyInfoLable">\s*<div\s*class="cell">([^<]+)<\/div>\s*<div\s*class="cell">([^<]+)<\/div>\s*<div\s*class="cell">([^<]+)<\/div>\s*<\/div>\s*<div\s*class="companyInfoVlaue">\s*<div\s*class="cell">([^<]+)<\/div>\s*<div\s*class="cell">([^<]+)<\/div>\s*<div\s*class="cell">([^<]+)<\/div>\s*<\/div>\s*<\/div>\s*`
 
-	regexQuarterlyResults = `\s*<div\s*class="cell"\s*style="display:\s*block;">\s*<div\s*class="quarterly\s*\w*">[^<]+<span\s*[^>]+>\s*[^<]*\s*<\/span>\s*<\/div>\s*<div\s*class="eps">\s*([^<]+)\s*<\/div>\s*<div\s*class="epsChg">([^<]+)<\/div>\s*<div\s*class="sales">([^<]+)<\/div>\s*<div\s*class="salesChg\s*\w+">([^<]+)<\/div>\s*<\/div>\s*`
+	regexQuarterlyResults = `\s*<div\s*class="cell"\s*style="display:\s*block;">\s*<div\s*class="quarterly\s*\w*">[^<]+<span\s*[^>]+>\s*[^<]*\s*<\/span>\s*<\/div>\s*<div\s*class="eps\s*\w*">\s*([^<]+)\s*<\/div>\s*<div\s*class="epsChg\s*\w*">([^<]+)<\/div>\s*<div\s*class="sales\s*\w*">([^<]+)<\/div>\s*<div\s*class="salesChg\s*\w*">([^<]+)<\/div>\s*<\/div>\s*`
 
 	regexFunds = `\s*<div\s*class="cell">\s*<span>[^<]+<\/span>\s*<span>(\d+)<\/span>\s*<\/div>\s*`
 )
